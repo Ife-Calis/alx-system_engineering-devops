de47ed112cd8feed4adbb6b9660610d1a6b8aa17
Ife-Calis <calistusifechukwu39@gmail.com>
2024-01-19 19:19:56 -0800

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# installing a package using puppet
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
